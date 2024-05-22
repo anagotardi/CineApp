5096a906c548e24c1aaab6bd702b07f870e3ca9d
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-22 09:37:47 -0300

    img e json

--- cinema/src/Pages/Filmes.jsx
+++ cinema/src/Pages/Filmes.jsx
@@ -1,6 +1,21 @@
+import data from '../../arquivo.json'
+
 function Filmes() {
+    console.log(data)
     return (  
+        <>
         <h1>Filmes</h1>
+        {
+            data.map(
+                filmes => (
+                    <> 
+                    <h1 key={filmes.title}>{filmes.title}</h1>
+                    <img src={filmes.image} alt="" />
+                    </>
+                )
+            )
+        }
+        </>
     );
 }
 
