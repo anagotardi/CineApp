55f9ede77c4f58b8b44f1da7f45fa056c73dc2bd
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-29 09:34:31 -0300

    29/5

--- cinema/src/Pages/Filmes.jsx
+++ cinema/src/Pages/Filmes.jsx
@@ -1,20 +1,10 @@
-import data from '../../arquivo.json'
+//instalar tailwind css intellisense
 
 function Filmes() {
-    console.log(data)
     return (  
         <>
         <h1>Filmes</h1>
-        {
-            data.map(
-                filmes => (
-                    <> 
-                    <h1 key={filmes.title}>{filmes.title}</h1>
-                    <img src={filmes.image} alt="" />
-                    </>
-                )
-            )
-        }
+        
         </>
     );
 }
--- cinema/src/Pages/Home.jsx
+++ cinema/src/Pages/Home.jsx
@@ -1,6 +1,41 @@
+import data from '../../arquivo.json'
+
 function Home() {
     return ( 
-        <h1>Home</h1>
+        <> 
+        <div className='grid grid-cols-3 gap-3'>
+
+        {
+            data.map(
+                (filmes, index) =>(
+                    <div className='card' key={index}>
+                        <h1>{filmes.title}</h1>
+                        <img src={filmes.image}/>
+                    <div className='tags'>
+                    <div className='text'>
+                    {
+                        filmes.tags.map( tag =>(
+                            <span className="bg-pink-500 text-white  m-1" key={tag}>{tag}</span>
+                        ))
+                    }
+                    {
+                        filmes.text.map( text =>(
+                            <p className='p-2' key={text}>{text}</p>
+                        )
+                            )
+                    }
+                    </div>
+                    </div>
+                    </div>
+                    
+
+                    
+                )
+            )
+        }
+            </div>
+
+        </>
      );
 }
 
