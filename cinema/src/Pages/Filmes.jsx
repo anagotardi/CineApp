1d8c3d55f598d1a8ec3e007f75ac79ac1c624890
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2024-06-19 17:09:20 -0300

    detalhes

--- cinema/src/Pages/Filmes.jsx
+++ cinema/src/Pages/Filmes.jsx
@@ -62,7 +62,85 @@ function Filmes() {
             }
     
         </div>
+
+        <h1 className="flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-800">Romances Nacionais<FaHeart/></h1>
+
+        <div className="listaFilmes flex"> 
+            {
+                filmes.map(
+                    filme => (
+
+                <>
+                  
+                        <div className="flex bg-white m-auto p-auto">
+                    
+                              <div
+                                className="flex overflow-x-scroll pb-10 hide-scroll-bar"
+                              >
+                                <div
+                                  className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 "
+                                >
+                                  <div className="inline-block px-3">
+                                    <div
+                                      className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                                    >
+                                         <img src= {`https://image.tmdb.org/t/p/w300/${filme.backdrop_path}`}/>
+                                         
+                                            <h1 className="ml-20">{filme.title}</h1>
+                                            
+
+                                    </div>
+                                  </div>
+                                 
+                                </div>
+                              </div>
+                        </div>
+                        </>
+                    )
+                )
+            }
+    
+        </div>
+
+        <h1 className="flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-800">Romances Baseados em Livros<FaHeart/></h1>
         
+        <div className="listaFilmes flex"> 
+            {
+                filmes.map(
+                    filme => (
+
+                <>
+                  
+                        <div className="flex bg-white m-auto p-auto">
+                    
+                              <div
+                                className="flex overflow-x-scroll pb-10 hide-scroll-bar"
+                              >
+                                <div
+                                  className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 "
+                                >
+                                  <div className="inline-block px-3">
+                                    <div
+                                      className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                                    >
+                                         <img src= {`https://image.tmdb.org/t/p/w300/${filme.backdrop_path}`}/>
+                                         
+                                            <h1 className="ml-20">{filme.title}</h1>
+                                            
+
+                                    </div>
+                                  </div>
+                                 
+                                </div>
+                              </div>
+                        </div>
+                        </>
+                    )
+                )
+            }
+    
+        </div>
+
         </>
      );
 }
--- cinema/src/Pages/Home.jsx
+++ cinema/src/Pages/Home.jsx
@@ -2,42 +2,51 @@ import data from '../../arquivo.json';
 import { FaHeart } from "react-icons/fa";
 
 function Home() {
-    return ( 
-        <> 
-       <div className='grid grid-cols-3 gap-3'>
-       <img src='/10-coisas-que-eu-odeio-em-voce-poster.jpg' alt="poster" className="w-[100px] h-100" />
+    return (
+        <div className='grid grid-cols-3 gap-3'>
+            <div className="col-span-3">
+                <img
+                    src='/10-coisas-que-eu-odeio-em-voce-poster.jpg'
+                    alt="poster"
+                    className="w-full object-cover"
+                    style={{ height: 'calc(100vh - 200px)' }} // Ajuste a altura conforme necessário
+                />
+            </div>
 
             <div className="col-span-3 flex bg-[#DE3843] text-white text-7xl justify-center items-center p-8 space-x-8">
                 <span>Loves do mês</span>
                 <FaHeart />
             </div>
 
-            {
-                data.map((filmes, index) => (
-                    <div className='card bg-zinc-50 rounded-lg' key={index}>
-                        <h1>{filmes.title}</h1>
-                        <img className="w-[800px] h-[200px] object-cover rounded-lg m-2" src={filmes.image} alt={filmes.title} />
-                        <div className='tags'>
-                            <div className='text m-5'>
-                                <div className='title m-7'>
-                                    {
-                                        filmes.tags.map(tag => (
-                                            <span className="bg-[#DE3843] text-white m-1 p-1 rounded-lg" key={tag}>{tag}</span>
-                                        ))
-                                    }
-                                    {
-                                        filmes.text.map(text => (
-                                            <p className='p-2' key={text}>{text}</p>
-                                        ))
-                                    }
-                                </div>
+            
+
+            {data.map((filme, index) => (
+                <div className='card bg-zinc-50 rounded-lg' key={index}>
+                    <img
+                        className="w-full h-[200px] object-cover rounded-lg m-2"
+                        src={filme.image}
+                        alt={filme.title}
+                    />
+                    <div className='tags'>
+                        <div className='text m-5'>
+                            <div className='title m-7'>
+                                {filme.tags.map((tag, tagIndex) => (
+                                    <span
+                                        className="bg-[#DE3843] text-white m-1 p-1 rounded-lg"
+                                        key={tagIndex}
+                                    >
+                                        {tag}
+                                    </span>
+                                ))}
+                                {filme.text.map((text, textIndex) => (
+                                    <p className='p-2' key={textIndex}>{text}</p>
+                                ))}
                             </div>
                         </div>
                     </div>
-                ))
-            }
+                </div>
+            ))}
         </div>
-        </>
     );
 }
 
